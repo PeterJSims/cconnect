68eac177e59b066fcad420dc400a39fde841e699
Peter Sims <ragtimesims@gmail.com>
2019-11-05 15:19:37 -0500

    user and jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,83 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
 
-//@route    GET api/users
-//@desc     Test route
+//@route    POST api/users
+//@desc     Register user
 //@access   Public
-router.get('/', (req, res) => {
-	res.send('User route');
-});
+router.post(
+	'/',
+	[
+		check('name', 'Name is a required field').not().isEmpty(),
+		check('email', 'Email is a required field').isEmail(),
+		check('password', 'Password must be six or more characters').isLength({ min: 6 })
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		const { name, email, password } = req.body;
+
+		try {
+			//Grab user by email
+			let user = await User.findOne({ email });
+			//Check if user already exists
+			if (user) {
+				return res.status(400).json({ errors: [ { msg: 'User already exists' } ] });
+			}
+			//Get user's gravatar
+			const avatar = gravatar.url(email, {
+				//size
+				s: '200',
+				//rating(g, pg, r,)
+				r: 'pg',
+				//use simple default blank picture
+				d: 'mm'
+			});
+			user = new User({
+				name,
+				email,
+				avatar,
+				password
+			});
+
+			//Encrypt password
+			const salt = await bcrypt.genSalt(10);
+
+			user.password = await bcrypt.hash(password, salt);
+
+			//Save new user to MongoDB
+			await user.save();
+
+			//Create and return jsonwebtoken
+			const payload = {
+				user: {
+					//Below grabs the _id property from MongoDB via mongoose and simply replaces _id for id.
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				//ten hours below
+				{ expiresIn: 36000 },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send('Server error');
+		}
+	}
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 //Connect MongoDB
 connectDB();
 
+//Initialize Middleware
+
+//This is the new way of doing bodyParser import and app.use(bodyParser.json())
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
 	res.send('Connected!');
 });
@@ -16,7 +21,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Connected on port ${PORT}`);
