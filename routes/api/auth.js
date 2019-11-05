69b007cf0f35a5b5889cd6e75451c97f981322ac
Peter Sims <ragtimesims@gmail.com>
2019-11-05 16:20:29 -0500

    create authenticating middleware and GET auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+const auth = require('../../middleware/auth');
 
 //@route    GET api/auth
 //@desc     Test route
 //@access   Public
-router.get('/', (req, res) => {
-	res.send('Auth route');
+router.get('/', auth, async (req, res) => {
+	try {
+		//.select can help keep you from returning private things like passwords.
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server error');
+	}
 });
 
 module.exports = router;
