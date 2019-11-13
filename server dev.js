861bf089e60fd400fa15a4d3bdfe719e9e98f9ae
Peter Sims <ragtimesims@gmail.com>
2019-11-12 21:02:27 -0500

    deploy

--- server dev.js
+++ server dev.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path');
 
 const app = express();
 
@@ -12,25 +11,15 @@ connectDB();
 //This is the new way of doing bodyParser import and app.use(bodyParser.json())
 app.use(express.json({ extended: false }));
 
-// app.get('/', (req, res) => {
-// 	res.send('Connected!');
-// });
+app.get('/', (req, res) => {
+	res.send('Connected!');
+});
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
-//Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-	//Set static folder
-	app.use(express.static('client/build'));
-
-	app.length('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
 
 const PORT = process.env.PORT || 5000;
 
